866cd8ca7a4429a91f75bdcc026e3726c57a6ce2
Brittany Bellantoni <brittt2121@hotmail.com>
2018-01-28 18:03:36 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -19,8 +19,6 @@ puts "-----------"
 puts " #{board[3]} | #{board[4]} | #{board[5]} "
 puts "-----------"
 puts " #{board[6]} | #{board[7]} | #{board[8]} "
-puts " "
-puts " "
 end
 
 display_board(board0)
